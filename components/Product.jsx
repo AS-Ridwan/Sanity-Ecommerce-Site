9872d8a6b261062be532057a54e596d00b4bc980
hipopridwan <ridwanhipop@gmail.com>
2023-03-18 00:05:18 +0600

    products part of sanity ecommerce

--- components/Product.jsx
+++ components/Product.jsx
@@ -5,19 +5,19 @@ import { urlFor } from "lib/client";
 const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
-      {/* <Link href={`/product/${slug.current}`}>
+      <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           <img
             src={urlFor(image && image[0])}
-            width={250}
-            height={250}
+            width={300}
+            height={300}
             className="product-image"
             alt=""
           />
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
         </div>
-      </Link> */}
+      </Link>
     </div>
   );
 };
