43bd782eeb996e7d09e5024566782e3f79195882
hipopridwan <ridwanhipop@gmail.com>
2023-03-20 15:30:00 +0600

    sale banner part of sanity ecommerce

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -18,25 +18,21 @@ const FooterBanner = ({
 }) => {
   return (
     <>
-      {/* <div className="footer-banner-container">
-      <div className="banner-desc">
-        <div className="left">
-          <p>{discount}</p>
-          <h3>{largeText1}</h3>
-
-          <p>{saleTime}</p>
+      <div className="footer-banner-container">
+        <div className="banner-desc">
+          <div className="left">
+            <img src={urlFor(image)} className="footer-banner-image" alt="" />
+          </div>
+          <div className="right">
+            <h1>{largeText1}</h1>
+            <h3>{smallText}</h3>
+            <p>{desc}</p>
+            <Link href={`/product/${product}`}>
+              <button type="button">{buttonText}</button>
+            </Link>
+          </div>
         </div>
-        <div className="right">
-          <p>{smallText}</p>
-          <h3>{midText}</h3>
-          <p>{desc}</p>
-          <Link href={`/product/${product}`}>
-            <button type="button">{buttonText}</button>
-          </Link>
-        </div>
-        <img src={urlFor(image)} className="footer-banner-image" alt="" />
       </div>
-    </div> */}
     </>
   );
 };
